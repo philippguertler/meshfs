d24ee06355535425f4b08e3a2be0738b0f1f0d94
Philipp Gürtler <pg@gentics.com>
2019-05-16 19:10:07 +0200

    Add read and readdir for all root types

--- src/meshfs.ts
+++ src/meshfs.ts
@@ -1,5 +1,5 @@
 import program from 'commander';
-import fuseExpress, { FileType } from 'fuse-express';
+import fuseExpress, { FileType, ENOENT } from 'fuse-express';
 import rp from 'request-promise';
 
 const api = rp.defaults({
@@ -26,30 +26,50 @@ async function mount(dir: string) {
 
   const app = fuseExpress();
 
+  const rootTypes = [
+    "users",
+    "groups",
+    "roles",
+    "projects",
+    "schemas",
+    "microschemas"
+  ];
+
   app.ls("/", (req, res) => {
     console.log("ls called!", req.path);
-    res.send([
-      "users",
-      "groups",
-      "roles",
-      "projects",
-      "schemas",
-      "microschemas"
-    ].map(name => ({
+    res.send(rootTypes.map(name => ({
       name,
       mode: FileType.DIRECTORY | 0o755
     })));
   });
 
-  app.ls("/schemas", async (req, res) => {
-    const schemas = await api.get('/schemas');
-    res.send(schemas.data.map((schema: any) => ({
-      name: schema.name + ".json",
+  app.ls("/:type", async (req, res) => {
+    const type = req.params.type;
+    const entities = await api.get(`/${type}`);
+    res.send(entities.data.map((entity: any) => ({
+      name: name(entity) + ".json",
       mode: FileType.REGULAR_FILE | 0o644,
-      size: JSON.stringify(schema, undefined, 2).length,
-      mtime: new Date(schema.edited)
+      size: JSON.stringify(entity, undefined, 2).length,
+      mtime: new Date(entity.edited)
     })));
   })
 
+  app.read("/:type/:filename", async (req, res) => {
+    console.log("Reading file");
+    const { filename, type } = req.params
+    console.log(filename);
+    const match = /^(.*)\.json$/.exec(filename);
+    if (!match) {
+      return res.status(ENOENT).send("");
+    }
+    const entities = await api.get(`/${type}`);
+    const entity = entities.data.find((entity: any) => name(entity) === match[1]);
+    res.send(JSON.stringify(entity, undefined, 2));
+  })
+
   app.mount(dir);
+}
+
+function name(entity: any) {
+  return entity.name || entity.username;
 }
